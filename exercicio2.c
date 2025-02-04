621bb4fe4f3e5d50aee619cf24612707ce149a30
BrunoMaximo03 <brunomaximobt@gmail.com>
2025-02-04 17:09:32 -0300

    Segundo exercício de inverter a string

---
+++ exercicio2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char vetor[100];
+    int tamanho, inicio = 0, fim;
+    char aux;
+
+    printf("STRING: \n");
+    fgets(vetor, sizeof(vetor), stdin);
+
+    
+    vetor[strcspn(vetor, "\n")] = '\0';
+
+    tamanho = strlen(vetor);
+    fim = tamanho - 1;
+
+    
+    while (inicio < fim) {
+        aux = vetor[inicio];
+        vetor[inicio] = vetor[fim];
+        vetor[fim] = aux;
+        inicio++;
+        fim--;
+    }
+
+    printf("String invertida: %s\n", vetor);
+
+    return 0;
+}
