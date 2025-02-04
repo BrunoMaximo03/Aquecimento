f1df44608c9480ceaeea5ec9489493c990e584b2
BrunoMaximo03 <brunomaximobt@gmail.com>
2025-02-04 16:22:22 -0300

    Exercicio 1

--- aqueci.c
+++ aqueci.c
@@ -1,12 +1,22 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>  // Para usar isupper()
+#include <stdlib.h>  // Para usar malloc e free
 
 int main() {
-    char nome[10];
-    fflush(stdin);
-    fgets(nome,10,stdin);
 
-    printf("%s",nome);
+    char vetor[100];
+    printf("Digite a string:\n ");
+    fgets(vetor,sizeof(vetor),stdin);
 
+    vetor[strcspn(vetor, "\n")] = '\0';
+    int contagem = 0;
+    while(vetor[contagem] != '\0') {
+        contagem++;
+    }
+
+    printf("A string tem %d caracteres\n",contagem);
+
+
+    return 0;
 }
